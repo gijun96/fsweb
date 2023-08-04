62578361d2138bb6a102cde9f752d7f1bcbcc1c6
gijun96 <kj5733@naver.com>
2023-08-04 17:34:40 +0900

    0804

---
+++ 51_css/51_css/part2/script.js
@@ -0,0 +1,9 @@
+const atext = document.querySelector('popup_on');
+const exitBtn = document.querySelector('exit_btn');
+
+function e(){
+    priventEvent.e();
+    atext.addEventListener('click', ()=>{
+        document.querySelector('modal').computedStyleMap.display =`none`
+    })
+}

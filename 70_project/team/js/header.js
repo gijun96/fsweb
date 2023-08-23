548066a4b9e53fdc33b9b27fb04095b3e60f2f49
gijun96 <kj5733@naver.com>
2023-08-24 00:49:16 +0900

    0824

--- 70_project/team/js/header.js
+++ 70_project/team/js/header.js
@@ -1,14 +1,17 @@
 // header의 변경할 요소 지정
 const header = document.querySelector("#header_inner");
+
 // scrollEvent는 스크롤이 움직이는 걸 인식하고
 // 스크롤이 top에서부터 70만큼 움직이면 요소에 클래스 추가
 function scrollEvent() {
+  let scrollVal = document.documentElement.scrollTop;
   document.addEventListener("scroll", function () {
-    if (document.documentElement.scrollTop > 70) {
+    if (scrollVal > 70) {
       header.classList.add("scroll_down");
       header.classList.remove("header_inner");
+
       // 스크롤 top으로 부터 얼마나 내려오는지 식별
-      console.log(document.documentElement.scrollTop);
+      // console.log(document.documentElement.scrollTop);
     } else {
       // 스크롤이 Top에서부터 70보다 작을 떄
       header.classList.remove("scroll_down");

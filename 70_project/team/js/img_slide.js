160cca81fd4ac4bda3c511e7882a7746a4a1a734
gijun96 <kj5733@naver.com>
2023-08-23 17:48:31 +0900

    0823

--- 70_project/team/js/img_slide.js
+++ 70_project/team/js/img_slide.js
@@ -1,31 +1,29 @@
-const prev_btn = document.querySelector('.btn_l');
-const next_btn = document.querySelector('.btn_r');
+const prev_btn = document.querySelector(".btn_l");
+const next_btn = document.querySelector(".btn_r");
 
-const item_li = document.querySelector('.item_list');
-
+const item_li = document.querySelector(".item_list");
 
 let index = 0;
 
-prev_btn.addEventListener('click',() =>{
-    if(index === 0){
-        index =2;
-    }else{
-        index -= 1;
-    };
-    console.log(index);
-    
-    item_li.style.transform = `translateX(-${1320 * index}px)`;
- 
-})
+prev_btn.addEventListener("click", () => {
+  if (index === 0) {
+    index = 2;
+    // prev_btn.style.display = 'none';
+  } else {
+    index -= 1;
+  }
+  console.log(index);
 
-next_btn.addEventListener('click',() =>{
-    if(index ===2){
-        index = 0;
-    }else{
-        index += 1;
-    }
-    item_li.style.transform= `translateX(-${1320 * index}px)`;
-    
-    console.log(index);
-})
+  item_li.style.transform = `translateX(-${1320 * index}px)`;
+});
 
+next_btn.addEventListener("click", () => {
+  if (index === 2) {
+    index = 0;
+  } else {
+    index += 1;
+  }
+  item_li.style.transform = `translateX(-${1320 * index}px)`;
+
+  console.log(index);
+});

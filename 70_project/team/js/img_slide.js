cade0de82c022e00631dc0097f39cd48a3af0c30
gijun96 <kj5733@naver.com>
2023-08-24 11:55:57 +0900

    0824

--- 70_project/team/js/img_slide.js
+++ 70_project/team/js/img_slide.js
@@ -13,7 +13,7 @@ prev_btn.addEventListener("click", () => {
     index -= 1;
   }
   console.log(index);
-
+  indicator(index);
   item_li.style.transform = `translateX(-${1320 * index}px)`;
 });
 
@@ -24,6 +24,29 @@ next_btn.addEventListener("click", () => {
     index += 1;
   }
   item_li.style.transform = `translateX(-${1320 * index}px)`;
-
+  indicator(index);
   console.log(index);
 });
+
+
+// 인디케이터 클릭 시 translateX(78px);
+// sec3 인디케이터
+const cur_bar = document.querySelector('list_bar');
+
+
+const ind1 = document.querySelector('ind1');
+const ind2 = document.querySelector('ind2');
+const ind3 = document.querySelector('ind3');
+function indicator(){
+  ind1.addEventListener("click", () =>{
+    item_li.style.transform = `translateX(-${1320 * index}px)`;
+    cur_bar.style.transform = `translateX(-${78 * index}px)`
+    console.log("event 발생");
+  })
+  ind2.addEventListener('click', () =>{
+    cur_bar.style.transform = `translateX(-${78 * index}px)`
+  })
+  ind3.addEventListener('click', () =>{
+    cur_bar.style.transform = `translateX(-${78 * index}px)`;
+  })
+}

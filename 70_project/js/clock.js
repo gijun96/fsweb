a688c82171c448bf0192f79e315fc3ec179c68fd
gijun96 <kj5733@naver.com>
2023-08-25 10:51:20 +0900

    0825

---
+++ 70_project/js/clock.js
@@ -0,0 +1,29 @@
+
+function setClock(){
+    let dateinfo = new Date();
+    let hour = modifyNumber(dateinfo.getHours());
+    let min = modifyNumber(dateinfo.getMinutes());
+    let sec = modifyNumber(dateinfo.getSeconds());
+    let year = dateinfo.getFullYear();
+    // monthIndex를 반환해주기 때문에 1을 더해준다.
+    let month = dateinfo.getMonth()+1;
+    let date = dateinfo.getDate();
+    document.getElementById('time').innerHTML = hour + ":" + min;
+    document.getElementById('date').innerHTML = year + "년" + month + "월" + date + "일";
+
+}
+// parseInt() : 문자열을 숫자로 변환
+function modifyNumber(time){
+    if(parseInt(time)<10){
+        return "0" + time;
+    }
+    else
+        return time;
+    
+}
+window.onload = function(){
+    setClock();
+    // 1초마다 setClock 함수 실행
+    setInterval(setClock, 1000); 
+} 
+
--- 70_project/team/js/img_slide.js
+++ 70_project/team/js/img_slide.js
@@ -12,8 +12,7 @@ prev_btn.addEventListener("click", () => {
   } else {
     index -= 1;
   }
-  console.log(index);
-  indicator(index);
+  // console.log(index);
   item_li.style.transform = `translateX(-${1320 * index}px)`;
 });
 
@@ -24,29 +23,5 @@ next_btn.addEventListener("click", () => {
     index += 1;
   }
   item_li.style.transform = `translateX(-${1320 * index}px)`;
-  indicator(index);
-  console.log(index);
+  // console.log(index);
 });
-
-
-// 인디케이터 클릭 시 translateX(78px);
-// sec3 인디케이터
-const cur_bar = document.querySelector('list_bar');
-
-
-const ind1 = document.querySelector('ind1');
-const ind2 = document.querySelector('ind2');
-const ind3 = document.querySelector('ind3');
-function indicator(){
-  ind1.addEventListener("click", () =>{
-    item_li.style.transform = `translateX(-${1320 * index}px)`;
-    cur_bar.style.transform = `translateX(-${78 * index}px)`
-    console.log("event 발생");
-  })
-  ind2.addEventListener('click', () =>{
-    cur_bar.style.transform = `translateX(-${78 * index}px)`
-  })
-  ind3.addEventListener('click', () =>{
-    cur_bar.style.transform = `translateX(-${78 * index}px)`;
-  })
-}

4714fb65b7ce81e3ab2b1453696ecda5ef5f7781
gijun96 <kj5733@naver.com>
2023-08-30 17:48:49 +0900

    0830

---
+++ 72_1slide/script.js
@@ -0,0 +1,28 @@
+
+// {}: 객체(object)
+// 객체.메서드()
+// 자스 데이터 : 숫자, 문자열, 불린, 객체{}, ...
+/*
+  객체 정의
+  {
+    속성: 값,
+    속성: 값,
+    속성: 값,
+    속성: 값,
+    속성: 값;
+  }
+ */
+// 요소.메서드()
+// $(jquery선택자).bxSlider();
+
+
+$('.bxslider').bxSlider({
+  // auto: true,
+  autoControls: true,
+  stopAutoOnClick: true,
+  pager: true,
+  // slideWidth: 1200
+});
+
+const wrapper = document.querySelector(".bx-wrapper");
+wrapper.style.border= "none";

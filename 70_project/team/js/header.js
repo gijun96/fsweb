f9dbf1cd112b290dba90a587f24f9a8ed805302c
gijun96 <kj5733@naver.com>
2023-09-04 17:37:56 +0900

    0904

--- 70_project/team/js/header.js
+++ 70_project/team/js/header.js
@@ -47,3 +47,15 @@ header.addEventListener('mouseover', ()=>{
   header.classList.add("scroll_down");
   header.classList.remove("header_inner");
 })
+
+
+// login & join / show & hide
+const myPage = document.querySelector(".gnb_icon li:nth-of-type(2)");
+const myPage_login = document.querySelector(".mypage_login");
+
+myPage.addEventListener("mouseover", () =>{
+  myPage_login.style.display = "block";
+  myPage_login.addEventListener("mouseout", () =>{
+    myPage_login.style.display = "none";
+  })
+})

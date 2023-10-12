46b91be2096ef710103ad9085b770e2faae1bbb6
gijun96 <kj5733@naver.com>
2023-10-12 22:27:17 +0900

    1012

--- Java/src/exam/exam_62.java
+++ Java/src/exam/exam_62.java
@@ -4,18 +4,30 @@ import java.util.Scanner;
 
 public class exam_62 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String a= scanner.nextLine();
-        String b= scanner.nextLine();
-        String c= scanner.nextLine();
-        if (b.length() ==1){
-            b = "0"+b;
+        Scanner scan = new Scanner(System.in);
+        O: while (true) {
+            System.out.println("값을 입력하여 주세요. ex)1 2 3");
+            String str = scan.nextLine().replaceAll("[^0-9]", " ");
+            String[] strArray = str.split(" ");
+            for (int i= 0; i<strArray.length;i++) {
+                if(strArray[i].isEmpty() || strArray.length !=3) {
+                    System.out.println("값이 비었거나 숫자가 아닙니다.");
+                    continue O;
+                }else if (Integer.parseInt(strArray[i]) >0
+                        && Integer.parseInt(strArray[0]) <=3
+                        && Integer.parseInt(strArray[1])<=20
+                        && Integer.parseInt(strArray[2])<=999
+                ){
+                    strArray[0] =String.format("%1d", Integer.parseInt(strArray[0]));
+                    strArray[1] =String.format("%02d", Integer.parseInt(strArray[1]));
+                    strArray[2] =String.format("%03d", Integer.parseInt(strArray[2]));
+                    System.out.printf("%s", strArray[i]);
+                }else{
+                    System.out.println("값의 범위를 확인해주세요.\n 값을 다시 입력해주세요.");
+                    continue O;
+                }
+            }
+            break;
         }
-        if (c.length() ==1){
-            c = "00"+c;
-        }else if (c.length() ==2){
-            c= "0"+c;
-        }
-        System.out.println(a+b+c);
     }
 }
--- Java/src/exam/exam_65.java
+++ Java/src/exam/exam_65.java
@@ -5,15 +5,19 @@ import java.util.Scanner;
 public class exam_65 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-            System.out.println("정수를 입력하세요.");
+        while (true) {
+            System.out.println("정수a를 입력하세요.");
             int a = scanner.nextInt();
-            for (int i=1; i<a;i++){
-                int res = i;
-                if ((i%10)%3==0 && i%10 !=0){
-                    System.out.printf("%s ", "X");
-                }else{
-                    System.out.printf("%d ", res);
+            if (a > 0 && a <= 10) {
+                for (int i = 1; i <= a; i++) {
+                    if ((i % 10) % 3 == 0 && i % 10 != 0) System.out.printf("%s ", "X");
+                    else System.out.printf("%d ", i);
                 }
+            }else {
+                System.out.println("정수a는 1~10까지의 수 입니다.");
+                continue;
             }
+            break;
+        }
     }
 }

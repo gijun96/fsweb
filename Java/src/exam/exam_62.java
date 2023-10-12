61a9a124874e476f1c338c39d7438f93a51cdc58
gijun96 <kj5733@naver.com>
2023-10-12 22:38:02 +0900

    1012

--- Java/src/exam/exam_62.java
+++ Java/src/exam/exam_62.java
@@ -6,12 +6,12 @@ public class exam_62 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         O: while (true) {
-            System.out.println("값을 입력하여 주세요. ex)1 2 3");
+            System.out.println("3개의 숫자의 값을 입력하여 주세요. ex)1 2 3");
             String str = scan.nextLine().replaceAll("[^0-9]", " ");
             String[] strArray = str.split(" ");
             for (int i= 0; i<strArray.length;i++) {
                 if(strArray[i].isEmpty() || strArray.length !=3) {
-                    System.out.println("값이 비었거나 숫자가 아닙니다.");
+                    System.out.println("숫자를 확인해 주세요.");
                     continue O;
                 }else if (Integer.parseInt(strArray[i]) >0
                         && Integer.parseInt(strArray[0]) <=3
@@ -27,6 +27,7 @@ public class exam_62 {
                     continue O;
                 }
             }
+            System.out.println("실행을 종료합니다.");
             break;
         }
     }
